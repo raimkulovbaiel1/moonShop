f73750b485a938bd6540bdf2e869d34c8fab5194
baiel <raimkulovbaiel2@gmail.com>
2026-03-21 20:01:37 +0600

    refact/index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import  Hero  from "./Sections/Hero";
-import Hero_Banner from "./Sections/Hero_Banner";
+import Hero_Banner from "./Sections/Hero_Banner"; 
+import Page_2 from "./Sections/Page_2";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <Hero_Banner />
+      <Hero_Banner /> 
+      <Page_2/>
     </>
   );
 };
