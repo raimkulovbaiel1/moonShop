05dbc23ee606c7cccd9a2504c95e84587668f2fe
baiel <raimkulovbaiel2@gmail.com>
2026-03-30 21:08:16 +0600

    aadded/to About

--- src/pages/About/Sections/HeaderTtext/index.tsx
+++ src/pages/About/Sections/HeaderTtext/index.tsx
@@ -1,6 +1,21 @@
-export const index = () => {
-  return (
-    <div>index</div>
-  )
+import { type FC } from 'react';
+import './style.css';
+
+interface HeaderTextProps {
+  //  Определения свойства для компонента
 }
-export default index
+
+
+const HeaderText: FC<HeaderTextProps> = () => {
+  return (
+    <section className="header-text">
+      <h1 className="header-text__title">About Moon</h1>
+
+      <p className="header-text__paragraph">
+        Moon's handmade ceramic products have been around since 1650, let's explore our journey
+      </p>
+    </section>
+  );
+};
+
+export default HeaderText;
